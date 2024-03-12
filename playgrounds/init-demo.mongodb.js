523469fc0390d8485138d1b97fce6289e363f4a4
Kyle Martin <kyle.martin@hotmail.co.nz>
2024-03-13 10:39:29 +1300

    Update init-demo.mongodb.js

--- playgrounds/init-demo.mongodb.js
+++ playgrounds/init-demo.mongodb.js
@@ -4,7 +4,6 @@ use('grocerystore');
 // Insert a few documents into the sales collection.
 db.getCollection('drinks').insertMany([
   { 'name': 'pepsi', 'price': 3, 'quantity': 10 },
-  { 'name': 'cocacola', 'price': 4, 'quantity': 10 },
-  { 'name': 'milk', 'price': 4, 'quantity': 10 },
+  { 'name': 'cocacola', 'price': 4, 'quantity': 10 }
 ]);
 
